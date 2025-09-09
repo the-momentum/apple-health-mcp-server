ad1ea0ff2729ffd1ef73156dc4437e316b0e5fe9
czajkub <czajkub@gmail.com>
2025-09-09 14:11:54 +0200

    fixed path in duckdb client

--- app/services/duck.py
+++ app/services/duck.py
@@ -1,19 +1,19 @@
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Any
 
 from duckdb import DuckDBPyRelation
 
 from app.config import settings
 
-
 @dataclass
 class DuckDBClient:
     def __init__(self):
-        self.parquetpath = f"{settings.DUCKDB_FILENAME}.parquet"
+        self.parquetpath: Path = Path(f"{settings.DUCKDB_FILENAME}.parquet")
 
     def __post_init__(self):
-        if not self.path.exists():
-            raise FileNotFoundError(f"XML file not found: {self.path}")
+        if not self.parquetpath.exists():
+            raise FileNotFoundError(f"XML file not found: {self.parquetpath}")
 
     @staticmethod
     def format_response(response: DuckDBPyRelation) -> list[dict[str, Any]]:
