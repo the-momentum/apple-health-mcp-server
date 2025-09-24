554d9a55c285d38737149981bf39f1792b3631c3
czajkub <czajkub@gmail.com>
2025-09-24 14:41:01 +0200

    tests

--- tests/mcptest.py
+++ tests/mcptest.py
@@ -1,20 +1,52 @@
-import pytest
+import asyncio
 
+import nest_asyncio
 from agents import Agent, Runner
-from agents.mcp import MCPServerStdio, MCPServerStdioParams
-
-params = MCPServerStdioParams(
-    command="uv",
-    args = [
-        "run",
-        "--directory",
-        "..",
-        "fastmcp",
-        "run",
-        "start.py"
-    ]
+from agents.mcp import (
+    MCPServerStreamableHttp,
+    MCPServerStreamableHttpParams,
 )
+from agents.model_settings import ModelSettings
+from dotenv import load_dotenv
 
-server = MCPServerStdio(params=params)
 
-agent = Agent()
+load_dotenv()
+nest_asyncio.apply()
+
+
+async def main() -> None:
+    # params = MCPServerStdioParams(
+    #     command="uv",
+    #     args = [
+    #         "run",
+    #         "--directory",
+    #         "../app",
+    #         "fastmcp",
+    #         "run",
+    #         "main.py"
+    #     ]
+    # )
+    #
+    # async with MCPServerStdio(
+    #     name="apple serwer",
+    #     params=params
+    # ) as server:
+
+    params = MCPServerStreamableHttpParams(
+        url="http://localhost:8000/mcp/",
+    )
+
+    async with MCPServerStreamableHttp(
+        params=params,
+    ) as server:
+        agent = Agent(
+            name="cwel",
+            mcp_servers=[server],
+            model_settings=ModelSettings(tool_choice="required"),
+        )
+        result = await Runner.run(agent, "give me a health summary from duckdb")
+        print(result.final_output)
+
+
+
+asyncio.run(main())
