a0a71a2c51401a2606245baa28c93d3d4007d82b
czajkub <czajkub@gmail.com>
2025-09-24 12:10:51 +0200

    lint

--- tests/mcptest.py
+++ tests/mcptest.py
@@ -1,21 +1,19 @@
 import asyncio
-import os
 
-import pytest
 import nest_asyncio
-from dotenv import load_dotenv
-
 from agents import Agent, Runner
-from agents.mcp import MCPServerStdio, MCPServerStdioParams, MCPServerStreamableHttp, MCPServerStreamableHttpParams
+from agents.mcp import (
+    MCPServerStreamableHttp,
+    MCPServerStreamableHttpParams,
+)
 from agents.model_settings import ModelSettings
-
+from dotenv import load_dotenv
 
 load_dotenv()
 nest_asyncio.apply()
 
 
-async def main():
-
+async def main() -> None:
     # params = MCPServerStdioParams(
     #     command="uv",
     #     args = [
@@ -34,19 +32,19 @@ async def main():
     # ) as server:
 
     params = MCPServerStreamableHttpParams(
-        url="http://localhost:8000/mcp/"
+        url="http://localhost:8000/mcp/",
     )
 
     async with MCPServerStreamableHttp(
         params=params,
     ) as server:
-
         agent = Agent(
             name="cwel",
             mcp_servers=[server],
-            model_settings=ModelSettings(tool_choice="required")
+            model_settings=ModelSettings(tool_choice="required"),
         )
         result = await Runner.run(agent, "give me a health summary from duckdb")
         print(result.final_output)
+
 
 asyncio.run(main())
