23ff176a464ebce5746765ba0d1c1490ef38596d
czajkub <czajkub@gmail.com>
2025-10-07 16:11:46 +0200

    uncomment mcp routerg imports

--- app/mcp/v1/mcp.py
+++ app/mcp/v1/mcp.py
@@ -6,5 +6,5 @@ mcp_router = FastMCP(name="Main MCP")
 
 mcp_router.mount(duckdb_reader.duckdb_reader_router)
 # mcp_router.mount(ch_reader.ch_reader_router)
-# mcp_router.mount(es_reader.es_reader_router)
-# mcp_router.mount(xml_reader.xml_reader_router)
+mcp_router.mount(es_reader.es_reader_router)
+mcp_router.mount(xml_reader.xml_reader_router)
